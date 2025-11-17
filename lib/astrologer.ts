ffacb3bbbc1072bc6a6772d946aee322f01af0d4
Abhi5453Abhi <abhi5453abhi@gmail.com>
2025-11-17 21:54:05 +0530

    Rename astrologer image to avoid URL encoding issues
    - Renamed from 'Screenshot 2025-11-17 at 9.50.01 PM.png' to 'astrologer-raghav-shastri.png'
    - Updated astrologer.ts to use new filename
    - Fixes 404 error in production due to spaces in filename

--- lib/astrologer.ts
+++ lib/astrologer.ts
@@ -2,7 +2,7 @@
 export const ASTROLOGER = {
   name: 'Astrologer Raghav Shastri',
   displayName: 'Raghav Shastri',
-  image: '/Screenshot 2025-11-17 at 9.50.01 PM.png',
+  image: '/astrologer-raghav-shastri.png',
   title: 'Pandit ji',
 }
 
